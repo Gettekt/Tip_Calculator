4f14fb88a3f9e3183aa4d92c8bed663569060397
Honors <honors@honorslapt-b59853.fios-router.home>
2018-12-13 18:14:26 -0500

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by Honors on 12/13/18.
+//  Copyright © 2018 ChristopherStudios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
